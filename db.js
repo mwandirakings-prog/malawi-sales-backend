bb860072aec8b6a0d3ef02959d6f6fb0d3aabcfa
Kings Mwandira <mwandirakings@gmail.com>
2026-05-21 07:39:10 +0100

    Fix Neon connection timeout settings

--- db.js
+++ db.js
@@ -3,14 +3,18 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
-pool.connect((err) => {
+pool.connect((err, client, release) => {
   if (err) {
     console.log('Database connection failed:', err.message);
   } else {
     console.log('Connected to Neon PostgreSQL successfully!');
+    release();
   }
 });
 
